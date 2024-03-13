3fa956d48a47ebe7953c356259b801903c7d36cb
Utkarsh <utkarshmark14@gmail.com>
2024-03-13 18:43:34 +0530

    added relevant comments for future reference

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,9 @@ const connectDB = async () => {
         const connectionInstance = await mongoose.connect(
             `${process.env.MONGODB_URI}/${DB_NAME}`
         );
+        /* For attempting DB connection, along with the DB_URI, we also need the DB_NAME (connected with a " / ")... 
+        That's why we stored the name in the " constants.js " file and we imported it here and used it for the connection.
+        */
 
         console.log(
             `\n MongoDB connected...✅ \n DB HOST: ${connectionInstance.connection.host}`
@@ -17,3 +20,11 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
+/* 
+-> Always remember, database is physically present usually in a different continent. Thus, it takes time for the data to reach us from the database. 
+That's why it is absolutely important to use " async and await " while writing the DB connection code.
+
+-> Also, there can be some conflicts while fetching data from the database. 
+That's why it is absolutely important to use the " try and catch " block while attempting to connect to the DB.
+*/
--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 // require("dotenv").config({path: './env'});
-import dotenv from "dotenv";
+
+/*
+-> Since our project begins from the index.js file, it is absolutely important to load all the env variables and make them available in the whole project. 
+That's why we set up the env variables ASAP in the index file. 
+
+-> There is nothing wrong in the approach commented in the top line of using the dotenv( it's just inconsistent ).., but now we can do it with import statements too, by just splitting the config code into two parts( I: import of dotenv & II: dotenv - config ) and enabling the experimental feature in the scripts of the package.json file. 
+*/
+
+import dotenv from "dotenv"; // Part I
 
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path: "./env",
-});
+}); // Part II
 
 connectDB();
 
 
 
 
+/*
+-> The code piece below can be used when you want put the whole database connection code in the " index.js " file only!
 
+-> There is nothing wrong in this approach, it's just that the index file gets a little cluttered!
 
+-> That's why we write the database connection code in a separate file, say " db/index.js " and then import that and execute it here ( connectDB() ) in the main " index.js " file.
+*/
 
-
-
-
-// When you put the whole database connection code in the index.js file only!
-// There is nothing wrong in this approach, it's just a little cluttered!
 
 /* 
 import express from "express";
