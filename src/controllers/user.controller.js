aa25a360282f6dee8c72a4203069b2cbcde4257d
Utkarsh <utkarshmark14@gmail.com>
2024-04-15 20:07:50 +0530

    added user account details management controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -289,4 +289,131 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 });
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await User.findById(req.user?._id);
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password!");
+    }
+
+    // updating the user's password with the new password provided in the request body
+    user.password = newPassword;
+
+    // saving the updated user object with validateBeforeSave set to false to bypass schema validation
+    await user.save({ validateBeforeSave: false });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Password changed successfully!"));
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+        .status(200)
+        .json(200, req.user, "Current user fetched successfully!");
+});
+
+// NOTE: it is advised to write separate controllers and endpoints for 'text-based updates' & for 'file updates'
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const { fullname, email } = req.body;
+
+    if (!fullname || !email) {
+        throw new ApiError(400, "All fields are required!");
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullname, // using ES6 syntax
+                email,
+                // email: email ...using old legacy syntax
+            },
+        },
+        { new: true }
+    ).select("-password");
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, user, "Account details updated successfully!")
+        );
+});
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+    const avatarLocalPath = req.file?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing!");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+    if (!avatar.url) {
+        throw new ApiError(
+            400,
+            "Some error occoured while uploading the avatar!"
+        );
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url,
+            },
+        },
+        { new: true }
+    ).select("-password");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user, "Avatar updated successfully!"));
+});
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+    const coverImageLocalPath = req.file?.path;
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover image file is missing!");
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!coverImage.url) {
+        throw new ApiError(
+            400,
+            "Some error occoured while uploading the cover image!"
+        );
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url,
+            },
+        },
+        { new: true }
+    ).select("-password");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user, "Cover image updated successfully!"));
+});
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+};
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,14 +1,30 @@
 // Purpose of this middleware: To verify if the user exists or not
 
+// This middleware function, verifyJWT, is designed to ensure the validity of a JSON Web Token (JWT) included in incoming requests. It serves the purpose of authenticating users and granting access to protected routes or resources only to authenticated users.
+
+// The main functionalities of this middleware are as follows:
+
+// 1. Extract Token: It attempts to extract the JWT from either the cookies or the Authorization header of the incoming request.
+
+// 2. Token Validation: If a token is present, it validates the token using the provided ACCESS_TOKEN_SECRET. If the token is invalid or expired, it throws an unauthorized error.
+
+// 3. User Verification: After successfully validating the token, it extracts the user ID from the decoded token and attempts to find the corresponding user in the database.
+
+// 4. User Assignment: If the user is found, it assigns the user object to the req object for future use in downstream middleware or route handlers.
+
+// 5. Error Handling: Any errors that occur during this process are caught and handled by throwing an ApiError with an appropriate status code and message.
+
+// Overall, this middleware enhances the security of the application by ensuring that routes protected by JWT authentication only allow access to authenticated users with valid tokens.
+
 import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
-
 // many a times, in our code we may use "req" and "next" but not "res"... in that case, in standard practice we replace the res with "_" and write "(req, _, next)"
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
+        // extracting token from either Cookies or Authorization header
         const token =
             req.cookies?.accessToken ||
             req.header("Authorization")?.replace("Bearer ", "");
@@ -17,8 +33,10 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
             throw new ApiError(401, "Unauthorized request!");
         }
 
+        // verifying the token using the ACCESS_TOKEN_SECRET
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
+        // finding user by ID extracted from decoded token
         const user = await User.findById(decodedToken?._id).select(
             "-password -refreshToken"
         );
@@ -27,9 +45,12 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
             throw new ApiError(401, "Invalid Access Token!");
         }
 
+        // assigning user object to req for future use in routes
         req.user = user;
-        next();
 
+        // calling the next middleware
+        next();
+        
     } catch (error) {
         throw new ApiError(401, error?.message || "Invalid access token!");
     }
