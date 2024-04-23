284c7b0795b45f3eeafb3e84b3877228bf0ed17d
Utkarsh <utkarshmark14@gmail.com>
2024-04-23 17:36:49 +0530

    added boilerplate code

--- src/app.js
+++ src/app.js
@@ -27,11 +27,18 @@ app.use(cookieParser());
 // at times, we want to access the cookies of our user's browser from our servers and also set it...
 // basically, it allows us to perform CRUD operations on user's cookies
 
-
-
 // routes import (not doing on the top, it's done like this usually)
-import userRouter from "./routes/user.routes.js"; 
+import userRouter from "./routes/user.routes.js";
 //--> we've ourselves named it "userRouter"
+
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
@@ -39,4 +46,12 @@ app.use("/api/v1/users", userRouter);
 //--> this means as soon as we hit "/api/v1/users", then the control gets passed to userRouter(ie, user.routes)... there we may write for "register", "login", ...
 //--> eg, "http://localhost:8000/api/v1/users/register"
 
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 export { app };
